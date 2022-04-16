bd197ebf4f0326a9dd9b0f827feb3c871ea2fc87
maoneart <maone.miui@gmail.com>
2022-04-16 07:07:56 +0700

    first commit

--- language/help.js
+++ language/help.js
@@ -6,7 +6,7 @@
 
 // Menu
 exports.menu = (botname, pushname) => {
-	return`Haii Kak ${pushname} 🐦
+	return`Haii Kak ${pushname} 
 
 Saya ${botname}, Bot Ini Adalah Beta Multi-Device WhatsApp.
 Jika Kamu Menemukan Semacam Bug Atau Kesalahan Mohon Dimaklumi Dulu Ya, Lapor Owner Agar Segera Di Perbaiki🙏`
